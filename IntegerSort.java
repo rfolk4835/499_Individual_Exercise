7151c5f40fdf485d5f027c0b796b9672c24422ba
rfolk4835 <reid_folk@yahoo.ca>
2021-09-17 03:01:05 -0700

    Integer Sort and Unit Test

---
+++ IntegerSort.java
@@ -0,0 +1,10 @@
+package arraySorting;
+
+import java.util.Arrays;
+
+public class IntegerSort {
+	public IntegerSort(int[] array){
+		Arrays.sort(array);
+		System.out.println(Arrays.toString(array));
+	}
+}
